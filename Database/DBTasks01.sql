feeb7ac455b5720cd0a65189ce8af3561d40c771
Yoshitoki <Yoshitoki@users.noreply.github.com>
2016-10-16 16:17:45 +0200

    Update DBTasks01.sql

--- Database/DBTasks01.sql
+++ Database/DBTasks01.sql
@@ -87,7 +87,19 @@ END;
 /
 
 -- Alternatív megoldás:
-
+-- CREATE OR REPLACE PROCEDURE tabla_kiiro(p_kar VARCHAR2) is
+--   CURSOR curs1 IS SELECT owner, table_name FROM dba_tables WHERE upper(table_name) LIKE upper(p_kar) || '%';
+--   rec curs1%ROWTYPE;
+-- BEGIN
+--   OPEN curs1;
+--   LOOP
+--     FETCH curs1 INTO rec;
+--     EXIT WHEN curs1%NOTFOUND;
+--     dbms_output.put_line(rec.owner || ' - ' || rec.table_name);
+--   END LOOP;
+--   CLOSE curs1;
+-- END;
+-- /
 
 set serveroutput on;
 call tabla_kiiro('z');
