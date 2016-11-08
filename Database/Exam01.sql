1f5e7fae6b825fd2f6a2f1545ff9eceafb45c8ce
Yoshitoki <Yoshitoki@users.noreply.github.com>
2016-11-08 20:37:10 +0100

    Update Exam01.sql

--- Database/Exam01.sql
+++ Database/Exam01.sql
@@ -1,3 +1,11 @@
+-- 1. feladat
+SELECT * FROM (SELECT partition_name, sum(bytes) sm_bytes FROM (SELECT partition_name, bytes
+FROM dba_segments
+NATURAL JOIN
+(SELECT partition_name p, bytes
+FROM dba_segments WHERE upper(segment_type) = 'TABLE SUBPARTITION')
+WHERE partition_name is not null AND upper(segment_type) = 'TABLE PARTITION') GROUP BY partition_name ORDER BY sm_bytes DESC) WHERE rownum < 4;
+
 -- 2. feladat
 SELECT * FROM
   (SELECT table_name, column_id, data_type FROM dba_tab_columns)
