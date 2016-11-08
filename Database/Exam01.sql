9886a0b0c996cead88184edd13f3f7b3815d3fe5
Yoshitoki <Yoshitoki@users.noreply.github.com>
2016-11-08 20:25:55 +0100

    Update Exam01.sql

--- Database/Exam01.sql
+++ Database/Exam01.sql
@@ -1,6 +1,8 @@
 -- 2. feladat
 SELECT * FROM
-(SELECT table_name, column_id, data_type FROM dba_tab_columns) JOIN
-(SELECT table_name tn, count(column_name) ccount FROM dba_tab_columns
-WHERE upper(owner) = 'NIKOVITS'
-GROUP BY table_name) ON table_name = tn WHERE column_id = ccount - 1 AND data_type = 'DATE';
+  (SELECT table_name, column_id, data_type FROM dba_tab_columns)
+ NATURAL JOIN
+  (SELECT table_name, count(column_name) ccount FROM dba_tab_columns
+  WHERE upper(owner) = 'NIKOVITS'
+  GROUP BY table_name)
+WHERE column_id = ccount - 1 AND data_type = 'DATE';
