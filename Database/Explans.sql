19d4e95f085a6340d37fbc41cecd0edff516aebc
Norbert Lestár <Yoshitoki@users.noreply.github.com>
2016-11-21 23:06:11 +0100

    Update Explans.sql

--- Database/Explans.sql
+++ Database/Explans.sql
@@ -11,3 +11,11 @@ SELECT /*+ HASH JOIN */ * FROM sh.countries co, sh.customers cu where co.country
 --    TABLE ACCESS + FULL + COUNTRIES                                    
 --    TABLE ACCESS + FULL + CUSTOMERS
 SELECT /*+ use_nl(co, cu) */ * FROM sh.countries cu join sh.customers co on cu.country_id = co.country_id;
+
+-- SELECT STATEMENT +  +                                                  
+--  MERGE JOIN +  +                                                      
+--    TABLE ACCESS + BY INDEX ROWID + COUNTRIES                          
+--      INDEX + FULL SCAN + COUNTRIES_PK                                 
+--    SORT + JOIN +                                                      
+--      TABLE ACCESS + FULL + CUSTOMERS
+SELECT /*+ USE_MERGE(cu, co) INDEX(cu) */ * FROM sh.countries cu join sh.customers co on cu.country_id = co.country_id;
